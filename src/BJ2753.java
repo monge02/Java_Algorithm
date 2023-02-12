d5cd68765f4a30460c825f26971934f2eebb4f69
Sang Hun <cho50058@gmail.com>
2023-02-13 05:54:46 +0900

    BJ2753 leap year

---
+++ src/BJ2753.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BJ2753 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if(num % 4 == 0 && num % 100 != 0) {
+            System.out.println("1");
+        } else if (num % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        sc.close();
+    }
+}
