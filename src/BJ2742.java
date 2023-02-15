c7c285b864158dbbf05c9dd47ed97daf4404f365
Sang Hun <cho50058@gmail.com>
2023-02-15 10:17:30 +0900

    BJ2742 54321

---
+++ src/BJ2742.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BJ2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
